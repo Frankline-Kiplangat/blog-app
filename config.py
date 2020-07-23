0e9d933ad797b8f9df72e6b56e82747bd4aae5e9
Frank <kipfrankline@gmail.com>
2020-07-23 11:43:13 +0300

    Add general changes

--- config.py
+++ config.py
@@ -1,3 +1,42 @@
-MAIL_USERNAME=kipfrankline@gmail.com
-MAIL_PASSWORD=frankdux32849179
-SECRET_KEY=1234
+import os
+import os
+
+class Config:
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    SUBJECT_PREFIX = 'myblog'
+    SENDER_EMAIL = 'kipfrankline@gmail.com'
+    
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
+        
+class ProdConfig(Config):
+    DEBUG = False
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/myblog_test'
+
+class DevConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
